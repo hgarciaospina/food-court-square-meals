286262a3e27ed8986bc4dad51c1be8cb99f77df7
Henry García <henrygarciaospina@gmail.com>
2023-05-16 21:40:12 -0500

    Feign implementation to validate restaurant creation stays with owner role.

--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/dto/response/RestaurantResponseDto.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/dto/response/RestaurantResponseDto.java
@@ -1,15 +1,26 @@
 package com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.response;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class RestaurantResponseDto {
+    @Pattern(regexp =  "^[a-zA-Z\\d\\s]*$")
     private String name;
+    @NotEmpty
+    @Size(min = 9)
+    @Pattern(regexp = "[1-9]\\d{8}")
     private String tin;
+    @NotEmpty
     private String address;
     private Long idOwner;
+
+    @Pattern(regexp = "^(\\+57)?\\s?(\\d{1})?[\\s|-]?(\\d{3})?[\\s|-]?(\\d{3})[\\s|-]?(\\d{2})[\\s|-]?(\\d{2})$", message = "Number phone must be a valid number phone")
+    @NotBlank(message = "Number phone is required")
+    @NotNull(message = "Number phone number is required")
     private String phone;
+    @Pattern(regexp = "^(https?|ftp)://([\\w.-]+)(:\\d+)?(/\\S*)?$")
     private String urlLogo;
 }
--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/handlers/impl/RestaurantHandlerImpl.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/handlers/impl/RestaurantHandlerImpl.java
@@ -1,5 +1,6 @@
 package com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.handlers.impl;
 
+import com.pragma.powerup.squaremealsmicroservice.adapters.driven.feignclient.UserFeignClientImpl;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.request.RestaurantRequestDto;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.handlers.IRestaurantHandler;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.mapper.IRestaurantRequestMapper;
@@ -12,9 +13,14 @@ import org.springframework.stereotype.Service;
 public class RestaurantHandlerImpl implements IRestaurantHandler {
     private final IRestaurantServicePort restaurantServicePort;
     private final IRestaurantRequestMapper restaurantRequestMapper;
+    private final UserFeignClientImpl userFeignClient;
 
     @Override
     public void saveRestaurant(RestaurantRequestDto restaurantRequestDto) {
-        restaurantServicePort.saveRestaurant(restaurantRequestMapper.toRestaurant(restaurantRequestDto));
+        if(userFeignClient.isOwnerUser(restaurantRequestDto.getIdOwner())){
+            restaurantServicePort.saveRestaurant(restaurantRequestMapper.toRestaurant(restaurantRequestDto));
+        }
+        //TODO: Modify excpetion by exception correct when is not owner user
+        //throw new Exception();
     }
 }
