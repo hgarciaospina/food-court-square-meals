f01c07a1ab1262fd08bd88044dd80c84d1567d6c
Henry García <henrygarciaospina@gmail.com>
2023-05-22 20:01:53 -0500

    Validation annotations and regular expressions are added to Restaurant RequestDto and removed from RestaurantResponseDto.

--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/dto/request/RestaurantRequestDto.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/dto/request/RestaurantRequestDto.java
@@ -1,5 +1,8 @@
 package com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Builder
@@ -8,10 +11,20 @@ import lombok.*;
 @Getter
 @Setter
 public class RestaurantRequestDto {
+    @Pattern(regexp =  "^[a-zA-Z\\d\\s]*$")
     private String name;
+
     private String tin;
     private String address;
+    @NotBlank(message = "The owner is required")
+    @NotNull(message = "The owner is required")
     private Long idOwner;
+
+    @Pattern(regexp = "^(\\+57)?\\s?(\\d{1})?[\\s|-]?(\\d{3})?[\\s|-]?(\\d{3})[\\s|-]?(\\d{2})[\\s|-]?(\\d{2})$", message = "Number phone must be a valid number phone")
+    @NotBlank(message = "Number phone is required")
+    @NotNull(message = "Number phone number is required")
     private String phone;
+
+    @Pattern(regexp = "^(https?|ftp)://([\\w.-]+)(:\\d+)?(/\\S*)?$",message="The url logo must be a format valid" )
     private String urlLogo;
 }
--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/dto/response/RestaurantResponseDto.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/dto/response/RestaurantResponseDto.java
@@ -7,20 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class RestaurantResponseDto {
-    @Pattern(regexp =  "^[a-zA-Z\\d\\s]*$")
     private String name;
-    @NotEmpty
-    @Size(min = 9)
-    @Pattern(regexp = "[1-9]\\d{8}")
     private String tin;
-    @NotEmpty
     private String address;
     private Long idOwner;
-
-    @Pattern(regexp = "^(\\+57)?\\s?(\\d{1})?[\\s|-]?(\\d{3})?[\\s|-]?(\\d{3})[\\s|-]?(\\d{2})[\\s|-]?(\\d{2})$", message = "Number phone must be a valid number phone")
-    @NotBlank(message = "Number phone is required")
-    @NotNull(message = "Number phone number is required")
     private String phone;
-    @Pattern(regexp = "^(https?|ftp)://([\\w.-]+)(:\\d+)?(/\\S*)?$")
     private String urlLogo;
 }
