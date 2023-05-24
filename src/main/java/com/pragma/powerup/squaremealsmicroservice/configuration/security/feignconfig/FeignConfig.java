9f7e11ac49a662ce991120bf0fec439b6a7141d1
Henry García <henrygarciaospina@gmail.com>
2023-05-24 16:27:54 -0500

    implementation of the unit tests of the use case RestaurantE CaseTest.

--- src/main/java/com/pragma/powerup/squaremealsmicroservice/configuration/security/feignconfig/FeignConfig.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/configuration/security/feignconfig/FeignConfig.java
@@ -1,7 +1,6 @@
 package com.pragma.powerup.squaremealsmicroservice.configuration.security.feignconfig;
 
 import feign.RequestInterceptor;
-import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +8,8 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig {
     @Bean
     public RequestInterceptor requestInterceptor() {
-        return new RequestInterceptor() {
-            @Override
-            public void apply(RequestTemplate template) {
-                // Agregar los encabezados de autenticación necesarios
-                // por ejemplo, el token de acceso JWT
-                template.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.nZU_gPcMXkWpkCUpJceSxS7lSickF0tTImHhAR949Z-Nt69LgW8G6lid-mqd9B579tYM8C4FN2jdhR2VRMsjtA");
-            }
-        };
+        return template -> template
+                .header("Authorization",
+                        "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.nZU_gPcMXkWpkCUpJceSxS7lSickF0tTImHhAR949Z-Nt69LgW8G6lid-mqd9B579tYM8C4FN2jdhR2VRMsjtA");
     }
 }
