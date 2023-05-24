2c40acbdb995d70f7bfaaba03deb5e47fc55265a
Henry García <henrygarciaospina@gmail.com>
2023-05-23 23:38:00 -0500

    The endpoint is updated to http://localhost:8090/user/owner.

---
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/configuration/security/feignconfig/FeignConfig.java
@@ -0,0 +1,21 @@
+package com.pragma.powerup.squaremealsmicroservice.configuration.security.feignconfig;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                // Agregar los encabezados de autenticación necesarios
+                // por ejemplo, el token de acceso JWT
+                template.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.nZU_gPcMXkWpkCUpJceSxS7lSickF0tTImHhAR949Z-Nt69LgW8G6lid-mqd9B579tYM8C4FN2jdhR2VRMsjtA");
+            }
+        };
+    }
+}
