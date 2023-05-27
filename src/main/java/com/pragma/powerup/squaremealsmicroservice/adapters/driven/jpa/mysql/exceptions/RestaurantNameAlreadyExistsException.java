a801ddfb9ec297849c9f9a52ce973140580393fa
Henry García <henrygarciaospina@gmail.com>
2023-05-27 00:19:17 -0500

    Modify name exception.

---
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driven/jpa/mysql/exceptions/RestaurantNameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.exceptions;
+
+public class RestaurantNameAlreadyExistsException extends RuntimeException {
+    public RestaurantNameAlreadyExistsException() {
+        super();
+    }
+}
