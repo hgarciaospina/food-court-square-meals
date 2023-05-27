c4687a8fca242f27e68f8809f113366c2a7347bc
Henry García <henrygarciaospina@gmail.com>
2023-05-27 00:34:40 -0500

    Defining constants for category exception messages.

--- src/main/java/com/pragma/powerup/squaremealsmicroservice/configuration/Constants.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/configuration/Constants.java
@@ -5,13 +5,19 @@ public class Constants {
     private Constants() {
         throw new IllegalStateException("Utility class");
     }
-
     public static final int TIN_NUMBER_LENGTH = 9;
+    public static final String NAME_PATTERN = "^[a-zA-Z\\s]+$";
+    public static final String DESCRIPTION_PATTERN = "^[a-zA-Z\\s]+$";
     public static final Long OWNER_ROLE_ID = 4L;
     public static final int MAX_PAGE_SIZE = 2;
     public static final String TIN_NUMBER_FORMAT = "^[1-9]\\d{0,8}$";
     public static final String DATE_TIME_FORMAT ="yyyy-MM-dd";
     public static final String RESPONSE_MESSAGE_KEY = "message";
+    public static final String CATEGORY_NAME_ALREADY_EXISTS_MESSAGE = "A category already exists with the name provided";
+    public static final String CATEGORY_NAME_INVALID_MESSAGE = "The category name must be alphabetic";
+    public static final String  CATEGORY_NAME_NULL_EMPTY_BLANK_INVALID_MESSAGE = "The category name cannot be null, blank or empty";
+    public static final String  CATEGORY_DESCRIPTION_NULL_EMPTY_BLANK_INVALID_MESSAGE = "The category description cannot be null, blank or empty";
+    public static final String CATEGORY_DESCRIPTION_INVALID_MESSAGE = "The category description must be alphabetic";
     public static final String RESTAURANT_CREATED_MESSAGE = "Restaurant created successfully";
     public static final String RESTAURANT_DELETED_MESSAGE = "Restaurant deleted successfully";
     public static final String RESPONSE_ERROR_MESSAGE_KEY = "error";
@@ -19,7 +25,7 @@ public class Constants {
     public static final String NO_DATA_FOUND_MESSAGE = "No data found for the requested petition";
     public static final String TIN_ALREADY_EXISTS_MESSAGE = "A restaurant already exists with the tin provided";
     public static final String  TIN_LENGTH_INVALID_MESSAGE = "The tin number must have 9 digits";
-    public static final String NAME_ALREADY_EXISTS_MESSAGE = "A restaurant already exists with the name provided";
+    public static final String RESTAURANT_NAME__ALREADY_EXISTS_MESSAGE = "A restaurant already exists with the name provided";
     public static final String OWNER_NOT_FOUND_MESSAGE = "No owner found with the id provided";
     public static final String OWNER_IS_NOT_MESSAGE = "Not a user with owner role or the user does not exist";
     public static final String RESTAURANT_NOT_FOUND_MESSAGE = "No restaurant found with the id provided";
