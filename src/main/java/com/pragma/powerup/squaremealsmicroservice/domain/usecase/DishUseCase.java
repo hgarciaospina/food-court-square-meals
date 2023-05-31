9fe82c995004c3d9fa6b40a4d9999cbe89a8eb65
Henry García <henrygarciaospina@gmail.com>
2023-05-31 18:21:57 -0500

    Definition initial of DishUseCase.

---
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/domain/usecase/DishUseCase.java
@@ -0,0 +1,19 @@
+package com.pragma.powerup.squaremealsmicroservice.domain.usecase;
+
+import com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.mappers.IDishEntityMapper;
+import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.request.DishRequestDto;
+import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.response.DishResponseDto;
+import com.pragma.powerup.squaremealsmicroservice.domain.api.IDishServicePort;
+import com.pragma.powerup.squaremealsmicroservice.domain.spi.IDishPersistencePort;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DishUseCase implements IDishServicePort {
+    private final IDishPersistencePort dishPersistencePort;
+    private final IDishEntityMapper dishEntityMapper;
+
+    @Override
+    public DishResponseDto saveDish(DishRequestDto dishRequestDto) {
+        return dishPersistencePort.saveDish(dishEntityMapper.requestDtoToModel(dishRequestDto));
+    }
+}
