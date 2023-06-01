5a60ecbee99d440631a0a7a8c021063248f1d47b
Henry García <henrygarciaospina@gmail.com>
2023-05-31 19:50:19 -0500

    Modify DishHandlerImpl.

--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/handlers/impl/DishHandlerImpl.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driving/http/handlers/impl/DishHandlerImpl.java
@@ -1,5 +1,9 @@
 package com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.handlers.impl;
 
+import com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.exceptions.RestaurantNameAlreadyExistsException;
+import com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.exceptions.RestaurantNotFoundException;
+import com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.repositories.ICategoryRepository;
+import com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.repositories.IRestaurantRepository;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.request.DishRequestDto;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.response.DishResponseDto;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.handlers.IDishHandler;
@@ -11,9 +15,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DishHandlerImpl implements IDishHandler {
     private final IDishServicePort dishServicePort;
+    private final IRestaurantRepository restaurantRepository;
+    private final ICategoryRepository categoryRepository;
 
     @Override
     public DishResponseDto saveDish(DishRequestDto dishRequestDto) {
+        if(!restaurantRepository.findById(dishRequestDto.getIdRestaurant()).isPresent()){
+          throw new RestaurantNotFoundException();
+        }
+
+        if(!categoryRepository.findById(dishRequestDto.getIdCategory()).isPresent()){
+            throw new RestaurantNameAlreadyExistsException();
+        }
+
+        if(!restaurantRepository.findById(dishRequestDto.getIdRestaurant()).isPresent()){
+            throw new RestaurantNotFoundException();
+        }
         return dishServicePort.saveDish(dishRequestDto);
     }
 }
