9a773d269319068a6a36e33206e442ae09807346
Henry García <henrygarciaospina@gmail.com>
2023-06-02 20:43:33 -0500

    Implementation mapper entityToModel and entityToresponseDto.

--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driven/jpa/mysql/mappers/IDishEntityMapper.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driven/jpa/mysql/mappers/IDishEntityMapper.java
@@ -1,22 +1,25 @@
 package com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.mappers;
 
-import com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.entity.CategoryEntity;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.entity.DishEntity;
-import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.request.CategoryRequestDto;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.request.DishRequestDto;
-import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.response.CategoryResponseDto;
 import com.pragma.powerup.squaremealsmicroservice.adapters.driving.http.dto.response.DishResponseDto;
-import com.pragma.powerup.squaremealsmicroservice.domain.model.Category;
 import com.pragma.powerup.squaremealsmicroservice.domain.model.Dish;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface IDishEntityMapper {
+    @Mapping(target = "categoryEntity", source = "dish.category")
+    @Mapping(target = "restaurantEntity", source = "dish.restaurant")
     DishEntity entityToModel(Dish dish);
     Dish modelToEntity(DishEntity dishEntity);
-    DishResponseDto responseDtoToEntity(DishEntity dishEntity);
+    @Mapping(target = "categoryName", source = "categoryEntity.name")
+    @Mapping(target = "restaurantName", source = "restaurantEntity.name")
+    DishResponseDto entityToResponseDto(DishEntity dishEntity);
     Dish requestDtoToModel(DishRequestDto dishRequestDto);
+    DishResponseDto requestDtoToResponseDto(DishRequestDto dishRequestDto);
+    Dish responseDtoToModel(DishResponseDto dishResponseDto);
 }
