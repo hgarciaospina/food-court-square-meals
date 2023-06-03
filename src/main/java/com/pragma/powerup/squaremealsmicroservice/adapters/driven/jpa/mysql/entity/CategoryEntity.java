64212f3250cbb67ff1e6d7c7c3a73b194c0b3b51
Henry García <henrygarciaospina@gmail.com>
2023-06-02 20:51:15 -0500

    Necessary annotations are added.

--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driven/jpa/mysql/entity/CategoryEntity.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driven/jpa/mysql/entity/CategoryEntity.java
@@ -1,10 +1,7 @@
 package com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driven/jpa/mysql/entity/DishEntity.java
+++ src/main/java/com/pragma/powerup/squaremealsmicroservice/adapters/driven/jpa/mysql/entity/DishEntity.java
@@ -1,10 +1,7 @@
 package com.pragma.powerup.squaremealsmicroservice.adapters.driven.jpa.mysql.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "dish")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class DishEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
